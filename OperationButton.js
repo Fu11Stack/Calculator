be53e673c403bcdf63d50ea5d20046a1de92698d
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2023-08-08 20:38:40 -0700

    Add files via upload

--- OperationButton.js
+++ OperationButton.js
@@ -1,11 +1,9 @@
 import { ACTIONS } from "./App"
 
-export default function DigitButton({ dispatch, operation }) {
-  return ( 
-    <button 
-      onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>
-    { operation }
-  
-  </button>
-  )
+export default function OperationButton({ dispatch, operation }) {
+ return (
+ <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}>
+  {operation}
+ </button>
+ )
 }
